f93dc749bf01ae0e203c5cb16d84a8d19a0a939c
nikolai <samohyn1@gmail.com>
2021-07-24 14:36:05 +0300

    Created container depoy actions configuration

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const packageJson = require('../package.json');
 
 
@@ -21,9 +20,6 @@ const devConfig = {
             },
             shared: packageJson.dependencies,
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
     ]
 }
 
